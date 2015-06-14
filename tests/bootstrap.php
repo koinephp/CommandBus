8c7a94161423c4af000592d3a4fe73dba3aa02a0
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2015-06-14 14:35:53 +0200

    Fixed autoloader stuff

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,5 @@ $autoloader = require_once dirname(__FILE__) . '/../vendor/autoload.php';
 
 if ($autoloader instanceof \Composer\Autoload\ClassLoader) {
     $autoloader->register();
+    $autoloader->add('Dummy', 'tests');
 }
-
-$autoloader->add('Dummy', 'tests');

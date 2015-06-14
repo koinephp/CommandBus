ca7bff1e83b561fd9fb83b00fa6865cee91eabe0
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2015-06-14 15:08:38 +0200

    Attempt to fix loading of dummy classes in travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,4 +5,17 @@ $autoloader = require_once dirname(__FILE__) . '/../vendor/autoload.php';
 if ($autoloader instanceof \Composer\Autoload\ClassLoader) {
     $autoloader->register();
     $autoloader->add('Dummy', 'tests');
+} else {
+    $dir = dirname(__FILE__) . '/Dummy/Command';
+    requireRecursive($dir);
+}
+
+function requireRecursive($dir)
+{
+    foreach (scandir($dir) as $filename) {
+        $path = $dir . '/' . $filename;
+        if (is_file($path)) {
+            require_once $path;
+        }
+    }
 }
